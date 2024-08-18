bbe2125c0377547a22936a960307a97472b7aa13
Manizha-khorram <108601629+Manizha-khorram@users.noreply.github.com>
2024-08-17 23:31:30 -0400

    fixed build issue

--- app/home/page.js
+++ app/home/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { LampContainer } from "../components/lapm";
+import { LampContainer } from "../components/lamp";
 
 export default function LampDemo() {
   return <LampContainer />;
